518b76f28c1d1682a52398cb668a4a6d304b3e0a
Evstrem <kataevstremskaa@gmail.com>
2024-12-14 14:46:18 +0700

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
 #include <pthread.h>
-#include <unistd.h> 
+#include <unistd.h>
+
 using namespace std;
+
 pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-int ready = 0; 
+int ready = 0;
+int processing = 0; // Флаг, показывает, обрабатывает ли потребитель событие
+
+
 void* providerThread(void*) {
     while (true) {
         pthread_mutex_lock(&lock);
-        if (ready == 1) {
+        if (ready == 1 || processing == 1) { 
             pthread_mutex_unlock(&lock);
-            continue;}
+            cout << "Поставщик: Потребитель занят, событие отложено." << endl;
+            continue;
+        }
         ready = 1;
+        processing = 1; // Указываем, что событие передано потребителю
         cout << "Поставщик: событие отправлено." << endl;
-        pthread_cond_signal(&cond1);        
-        pthread_mutex_unlock(&lock);        
-        sleep(1);}
-    return nullptr;}
+        pthread_cond_signal(&cond1);
+        pthread_mutex_unlock(&lock);
+        sleep(1);
+    }
+    return nullptr;
+}
+
 void* consumerThread(void*) {
     while (true) {
         pthread_mutex_lock(&lock);
         while (ready == 0) {
             pthread_cond_wait(&cond1, &lock);
-            cout << "Потребитель: событие получено." << endl;}
+            cout << "Потребитель: событие получено." << endl;
+        }
         ready = 0;
-        cout << "Потребитель: событие обработано." << endl;        
-        pthread_mutex_unlock(&lock); }
-    return nullptr;}
+        processing = 0; // Указываем, что потребитель завершил обработку
+        cout << "Потребитель: событие обработано." << endl;
+        pthread_mutex_unlock(&lock);
+    }
+    return nullptr;
+}
+
 int main() {
     pthread_t provider, consumer;
     pthread_create(&provider, nullptr, providerThread, nullptr);
@@ -35,4 +51,6 @@ int main() {
     pthread_join(consumer, nullptr);
     pthread_mutex_destroy(&lock);
     pthread_cond_destroy(&cond1);
-    return 0;}
+    return 0;
+}
+
